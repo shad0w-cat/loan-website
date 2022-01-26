56727dc0f9a3a69cd986ddac463ed03136b72b9b
Nandini Bisht <nandinibisht296@gmail.com>
2022-01-26 13:44:25 +0530

    My commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const selectedRange =document.getElementById("selectedRange");
+const selectedRange2 =document.getElementById("selectedRange2");
+
+function result()
+{
+    let interest = 20;
+    let amt = selectedRange.textContent;
+    
+}
+
+function rangeSlider1(value)
+{
+    selectedRange.textContent = value;
+    
+}
+function rangeSlider2(value)
+{
+    selectedRange2.textContent = value;
+}
